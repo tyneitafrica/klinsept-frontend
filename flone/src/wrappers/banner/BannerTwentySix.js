c8195e697e685a2a167b9ae808ea6a7cef3dc424
steve nene <stevekid705@gmail.com>
2024-11-05 20:00:16 +0300

    added gallery section

--- flone/src/wrappers/banner/BannerTwentySix.js
+++ flone/src/wrappers/banner/BannerTwentySix.js
@@ -1,77 +1,72 @@
 import PropTypes from "prop-types";
 import React from "react";
 import { Link } from "react-router-dom";
+import SectionTitle from "../../components/section-title/SectionTitle";
 
 const BannerTwentySix = ({ spaceBottomClass }) => {
+  // Define banner data as an array of objects
+  const banners = [
+    {
+      image: `${process.env.PUBLIC_URL}/assets/img/banner/banner-4.jpg`,
+      title: "Covid - 19",
+      subtitle: "Medix Mask",
+      discount: "Up To 40% Off",
+      link: "/shop-grid-standard",
+    },
+    {
+      image: `${process.env.PUBLIC_URL}/assets/img/banner/banner-7.jpg`,
+      title: "",
+      subtitle: "Hand Gloves",
+      discount: "Up To 30% Off",
+      link: "/shop-grid-standard",
+    },
+    {
+      image: `${process.env.PUBLIC_URL}/assets/img/banner/banner-3.jpg`,
+      title: "Covid - 19",
+      subtitle: "Hand Sanitizer",
+      discount: "Up To 40% Off",
+      link: "/shop-grid-standard",
+    },
+  ];
+
   return (
     <div className={`banner-area ${spaceBottomClass ? spaceBottomClass : ""}`}>
+      {/* Section Title */}
+      <SectionTitle
+        titleText="Gallery section"
+        subtitleText=""
+        subtitleColorClass="text-muted" // Optional custom class for subtitle
+        positionClass="text-center"
+        spaceClass="mb-5"
+      />
       <div className="container-fluid p-0">
         <div className="row no-gutters">
-          <div className="col-width-banner33-1">
-            <div className="single-banner mb-30">
-              <a href="product-details.html">
-                <img
-                  src={
-                    process.env.PUBLIC_URL + "/assets/img/banner/banner-43.png"
-                  }
-                  alt=""
-                />
-              </a>
-              <div className="banner-content-33-2 banner-content-33-2-position1">
-                <h4>Covid - 19</h4>
-                <h2>
-                  Medix Mask <br />
-                  Up To 40% Off
-                </h2>
-                <Link to={process.env.PUBLIC_URL + "/shop-grid-standard"}>
-                  Shop Now
-                </Link>
+          {banners.map((banner, index) => (
+            <div
+              className={`col-width-banner33-${index % 2 === 0 ? "1" : "2"}`}
+              key={index}
+            >
+              <div className="single-banner mb-30">
+                <a href="product-details.html">
+                  <img src={banner.image} alt="" />
+                </a>
+                <div
+                  className={`banner-content-33-2 banner-content-33-2-position${
+                    index + 1
+                  }`}
+                >
+                  {banner.title && <h4>{banner.title}</h4>}
+                  <h2>
+                    {banner.subtitle} <br />
+                    {banner.discount}
+                  </h2>
+                  <Link to={process.env.PUBLIC_URL + banner.link}>
+                    Shop Now
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-width-banner33-2">
-            <div className="single-banner mb-30">
-              <a href="product-details.html">
-                <img
-                  src={
-                    process.env.PUBLIC_URL + "/assets/img/banner/banner-44.png"
-                  }
-                  alt=""
-                />
-              </a>
-              <div className="banner-content-33-2 banner-content-33-2-position2">
-                <h2>
-                  Hand Gloves <br />
-                  Up To 30% Off
-                </h2>
-                <Link to={process.env.PUBLIC_URL + "/shop-grid-standard"}>
-                  Shop Now
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div className="col-width-banner33-1">
-            <div className="single-banner mb-30">
-              <a href="product-details.html">
-                <img
-                  src={
-                    process.env.PUBLIC_URL + "/assets/img/banner/banner-45.png"
-                  }
-                  alt=""
-                />
-              </a>
-              <div className="banner-content-33-2 banner-content-33-2-position3">
-                <h4>Covid - 19</h4>
-                <h2>
-                  Hand Sanitizer <br />
-                  Up To 40% Off
-                </h2>
-                <Link to={process.env.PUBLIC_URL + "/shop-grid-standard"}>
-                  Shop Now
-                </Link>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
@@ -79,7 +74,7 @@ const BannerTwentySix = ({ spaceBottomClass }) => {
 };
 
 BannerTwentySix.propTypes = {
-  spaceBottomClass: PropTypes.string
+  spaceBottomClass: PropTypes.string,
 };
 
 export default BannerTwentySix;
