ee9e854409bf1d8825d26f73426a454153329916
Stephen Nene <stephen.nene@student.moringaschool.com>
2024-11-10 16:11:38 +0300

    using the json for data

--- flone/src/redux/actions/productActions.js
+++ flone/src/redux/actions/productActions.js
@@ -1,4 +1,4 @@
-
+import  products from "../../data/products.json"
 import axios from "axios";
 export const FETCH_PRODUCTS_SUCCESS = "FETCH_PRODUCTS_SUCCESS";
 
@@ -14,7 +14,7 @@ export const fetchProducts = () => {
     try {
       const response = await axios.get("http://localhost:3000/products.json"); // Replace with your API endpoint
       // console.log(response.data)
-      dispatch(fetchProductsSuccess(response.data));
+      dispatch(fetchProductsSuccess(products));
     } catch (error) {
       console.error("Failed to fetch products:", error);
       // Optional: Dispatch an error action for error handling
