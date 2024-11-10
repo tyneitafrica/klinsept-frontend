43ac419e0e56d475080bd1cca9a788568880ee61
Stephen Nene <stephen.nene@student.moringaschool.com>
2024-11-10 16:18:14 +0300

    removed network request

--- flone/src/redux/actions/productActions.js
+++ flone/src/redux/actions/productActions.js
@@ -7,17 +7,22 @@ const fetchProductsSuccess = products => ({
   payload: products
 });
 
-
-// Fetch products from server
 export const fetchProducts = () => {
-  return async dispatch => {
-    try {
-      const response = await axios.get("http://localhost:3000/products.json"); // Replace with your API endpoint
-      // console.log(response.data)
-      dispatch(fetchProductsSuccess(products));
-    } catch (error) {
-      console.error("Failed to fetch products:", error);
-      // Optional: Dispatch an error action for error handling
-    }
+  return dispatch => {
+    dispatch(fetchProductsSuccess(products)); // Use local mock data
   };
 };
+
+// Fetch products from server
+// export const fetchProducts = () => {
+//   return async dispatch => {
+//     try {
+//       dispatch(fetchProductsSuccess(products));
+//       const response = await axios.get("http://localhost:3000/products.json"); // Replace with your API endpoint
+//       // console.log(response.data)
+//     } catch (error) {
+//       console.error("Failed to fetch products:", error);
+//       // Optionally Dispatch an error action for error handling
+//     }
+//   };
+// };
