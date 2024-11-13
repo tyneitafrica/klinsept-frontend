69d7a6f4ea5213d365e5dfc026bad8ef8741d5d7
Stephen Nene <stephen.nene@student.moringaschool.com>
2024-11-13 11:13:56 +0300

    adding a new roducts card

--- flone/src/pages/Search.js
+++ flone/src/pages/Search.js
@@ -10,7 +10,6 @@ import { addToWishlist } from "../redux/actions/wishlistActions";
 import { addToCompare } from "../redux/actions/compareActions";
 import '../assets/css/search.css'
 
-
 const Search = ({ cartItems, wishlistItems, compareItems, addToCart, addToWishlist, addToCompare }) => {
   const { searchParams } = useParams();
 const dummyProducts = Array(15).fill({
