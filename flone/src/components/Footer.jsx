9be7252fca50b4f27ae98a895026b5738bcd4c52
Stephen Nene <stephen.nene@student.moringaschool.com>
2024-11-15 09:14:52 +0300

    footer restyled

--- flone/src/components/Footer.jsx
+++ flone/src/components/Footer.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "../assets/css/Footer.css"; // Import the CSS file
 import {
   FaTiktok,
   FaXTwitter,
@@ -7,145 +6,71 @@ import {
   FaGithub,
   FaFacebookF,
 } from "react-icons/fa6";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import FooterNewsletter from "./footer/FooterNewsletter";
+import "../assets/css/Footer.css"; // Import the CSS file
 
 export default function Footer({ darkMode = !true }) {
   return (
     <footer className={darkMode ? "bg-dark" : "bg-light"}>
       <div className="container">
         <div className="footer-grid">
-          <div>
+          <div className="section-links">
             <h2 className={`section-title`}>Company</h2>
-            <ul>
-              <li>
-                <Link className="section-link" to="/about">
-                  About
-                </Link>
-              </li>
-              <li>
-                <Link className="section-link" to="/contact">
-                  Contact Us
-                </Link>
-              </li>
-              <li>
-                <Link className="section-link" to="#">
-                  Brand Center
-                </Link>
-              </li>
-              <li>
-                <Link className="section-link" to="/blogs">
-                  Blog
-                </Link>
-              </li>
-            </ul>
+
+            <NavLink to="/about">About</NavLink>
+
+            <NavLink to="/contact">Contact Us</NavLink>
+
+            <NavLink to="/products">products</NavLink>
+
+            <NavLink to="/blogs">Blog</NavLink>
           </div>
-          <div>
+          <div className="section-links">
             <h2 className="section-title">Help center</h2>
-            <ul>
-              <li>
-                <a className="section-link" href="/">
-                  Discord Server
-                </a>
-              </li>
-              <li>
-                <a className="section-link" href="/">
-                  Twitter
-                </a>
-              </li>
-              <li>
-                <a className="section-link" href="/">
-                  Facebook
-                </a>
-              </li>
-              <li>
-                <a className="section-link" href="/">
-                  Contact Us
-                </a>
-              </li>
-            </ul>
+
+            <Link href="/">FAQs</Link>
+
+            <NavLink to="/contact">Contact Us</NavLink>
+
+            <Link href="/">Twitter</Link>
+
+            <Link href="/">Facebook</Link>
           </div>
-          <div>
+          <div className="section-links">
             <h2 className="section-title">Categories</h2>
-            <ul>
-              <li>
-                <Link className="section-link" to="/products">
-                  Antiseptics
-                </Link>
-              </li>
-              <li>
-                <Link className="section-link" to="/products">
-                  Detergents
-                </Link>
-              </li>
-              <li>
-                <Link className="section-link" to="/products">
-                  Handwash
-                </Link>
-              </li>
-              <li>
-                <Link className="section-link" to="/products">
-                  Disinfectants
-                </Link>
-              </li>
-            </ul>
+            <Link to="/products">Antiseptics</Link>
+            <Link to="/products">Detergents</Link>
+            <Link to="/products">Handwash</Link>
+            <Link to="/products">Disinfectants</Link>{" "}
           </div>
           <div>
+            <h3 className="section-title">SUBSCRIBE</h3>
             <FooterNewsletter />
           </div>
         </div>
-        <div className={`text-center `}>
-          <span>
-            © 2024{" "}
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://klinsept.vercel.app/"
-            >
-              Klinsept
-            </a>
-            . All Rights Reserved.
-          </span>
-          <div className="social-icons">
-            <Link
-              to="/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="social-icon-link"
-            >
-              <FaFacebookF />
-            </Link>
-            <Link
-              to="/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="social-icon-link"
-            >
-              <FaInstagram />
-            </Link>
-            <Link
-              target="_blank"
-              className="social-icon-link"
-              rel="noopener noreferrer"
-            >
-              <FaXTwitter />
-            </Link>
-            <Link
-              to="/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="social-icon-link"
-            >
-              <FaGithub />
-            </Link>
-            <Link
-              to="/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="social-icon-link"
-            >
-              <FaTiktok />
-            </Link>
+        <div className="footer">
+          <div className="text-center flex justify-between items-center">
+            <span className="copyright">
+              © 2024 <Link to="/">Klinsept sprl</Link>. All Rights Reserved.
+            </span>
+            <div className="social-icons">
+              <Link to="/" target="_blank">
+                <FaFacebookF />
+              </Link>
+              <Link to="/" target="_blank">
+                <FaInstagram />
+              </Link>
+              <Link target="_blank">
+                <FaXTwitter />
+              </Link>
+              <Link to="/" target="_blank">
+                <FaGithub />
+              </Link>
+              <Link to="/" target="_blank">
+                <FaTiktok />
+              </Link>
+            </div>
           </div>
         </div>
       </div>
--- flone/src/components/footer/FooterNewsletter.js
+++ flone/src/components/footer/FooterNewsletter.js
@@ -49,9 +49,7 @@ const FooterNewsletter = ({ sideMenu }) => {
 
   return (
     <div className={`footer-widget mb-30 ${sideMenu ? "ml-ntv5" : "ml-70"}`}>
-      <div className="footer-title">
-        <h3>SUBSCRIBE</h3>
-      </div>
+
       <div className="subscribe-style">
         <p>Get E-mail updates about our latest shop and special offers.</p>
 
