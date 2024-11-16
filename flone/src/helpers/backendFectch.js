212398ddc3ad2109a0e8fe4d3edf756d8ad60fb2
Stephen Nene <stephen.nene@student.moringaschool.com>
2024-11-16 16:46:15 +0300

    updated the reat-toast

--- flone/src/helpers/backendFectch.js
+++ flone/src/helpers/backendFectch.js
@@ -2,18 +2,18 @@ import axios from 'axios';
 
 // Assuming your .env has the API URL and API key
 // const API_URL = 'http://192.168.1.61:8000'; 
-const API_URL = 'https://klinsept-backend.onrender.com/['
+const API_URL = 'https://klinsept-backend.onrender.com/'
 const API_KEY = 'f6c52669-b6a9-4901-8558-5bc72b7e983a';
 
 export const registerFetch = async (registerData) => {
   try {
     const response = await axios.post(
-      `${API_URL}/api/v1.0/auth/signin/`,  // Adjust your API endpoint here
+      `${API_URL}api/v1.0/auth/signin/`,  // Adjust your API endpoint here
       registerData,
       {
         headers: {
-          'x-api-key':`${API_KEY}`, 
-          'Content-Type': 'application/json',
+          'x-api-key':`f6c52669-b6a9-4901-8558-5bc72b7e983a`, 
+          // 'Content-Type': 'application/json',
         },
       }
     );
