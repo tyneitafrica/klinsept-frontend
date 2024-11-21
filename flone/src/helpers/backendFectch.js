8fd65f1e9bd5d4b17c4de4eb1b449e1cd495ef4a
Stephen Nene <stephen.nene@student.moringaschool.com>
2024-11-21 16:18:08 +0300

    added envs

--- flone/src/helpers/backendFectch.js
+++ flone/src/helpers/backendFectch.js
@@ -1,41 +1,28 @@
 import axios from "axios";
-// import { setUserData } from "../redux/actions/appAction";
 import { logoutUser } from "../redux/actions/appAction";
 
-
-// Use the environment variables
-const API_URL = "https://klinsept-backend.onrender.com/";
-// const API_URL = process.env.REACT_APP_API_URL;
-const API_KEY = "f6c52669-b6a9-4901-8558-5bc72b7e983a";
-// const API_URL2 = process.env.REACT_APP_API_URL2;
+const API_URL = process.env.REACT_APP_API_URL;
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const registerFetch = async (registerData) => {
-  const response = await axios.post(
-    `${API_URL}api/v1.0/auth/signin/`,registerData, {
-      headers: {
-        "x-api-key": API_KEY,
-      },
-    }    
-  );
-    return response;
-}
+  const response = await axios.post(`${API_URL}auth/signin/`, registerData, {
+    headers: {
+      "x-api-key": API_KEY,
+    },
+  });
+  return response;
+};
 
-    
 export const LoginFetch = async (loginData) => {
-    const response = await axios.post(
-      "https://klinsept-backend.onrender.com/api/v1.0/auth/login/",
-      loginData,
-      {
-        withCredentials: true,
-        headers: {
-          "x-api-key": API_KEY,
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    console.log(response.data.jwt)
-    return response
-    
+  const response = await axios.post(`${API_URL}/auth/login/`, loginData, {
+    withCredentials: true,
+    headers: {
+      "x-api-key": API_KEY,
+      "Content-Type": "application/json",
+    },
+  });
+  console.log(response.data.jwt);
+  return response;
 };
 
 export const forgotPassword = async (email) => {
@@ -113,7 +100,7 @@ export const getProducts = async () => {
         },
       }
     );
-    console.log(response.data)
+    console.log(response.data);
     return response.data;
   } catch (error) {
     console.error("Get Products error:", error);
