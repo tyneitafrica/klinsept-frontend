45f230255806cd60538d0b7faa0b7a0a377ce01f
Stephen Nene <stephen.nene@student.moringaschool.com>
2024-11-28 11:16:45 +0300

    more descritions added

--- flone/src/redux/actions/cartActions.js
+++ flone/src/redux/actions/cartActions.js
@@ -45,7 +45,7 @@ export const addToCart = (item, quantityCount = 1, size, order_type) => {
         size,
         order_type
       );
-      console.log(responseData);
+      // console.log(responseData);
 
       dispatch({
         type: ADD_TO_CART,
@@ -74,9 +74,9 @@ export const decreaseQuantity = (item, addToast) => {
 };
 //delete from cart
 export const deleteFromCart = (item) => {
-  console.log(item);
+  // console.log(item);
   return async (dispatch) => {
-    console.log(item);
+    // console.log(item);
     try {
       const response = await axios.delete(
         `${process.env.REACT_APP_API_URL}cart/remove/${item.product_id}`,
