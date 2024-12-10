286a78002a3076aaaaeff28cdf6c244e6f4fd150
Stephen Nene <stephen.nene@student.moringaschool.com>
2024-12-10 11:23:57 +0300

    added clear

--- flone/src/index.js
+++ flone/src/index.js
@@ -16,6 +16,7 @@ import * as serviceWorker from "./serviceWorker";
 import { Toaster } from "react-hot-toast";
 
 import { composeWithDevTools } from "redux-devtools-extension";
+window.addEventListener('load',()=>localStorage.clear())
 
 const store = createStore(
   rootReducer,
